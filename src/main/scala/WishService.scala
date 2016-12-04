1481045990ea31f982e6ba6f4a24d1148d8fba33
Gustav Behm <gustav.behm@gmail.com>
2016-12-04 15:27:39 +0100

    Implement WishService's /wishlist route

--- src/main/scala/WishService.scala
+++ src/main/scala/WishService.scala
@@ -1,16 +1,22 @@
+import java.time.ZonedDateTime
 import scalaz._, syntax.bind._
 import org.http4s.{AuthedService, AuthedRequest}
 import org.http4s.dsl._
 import io.circe.syntax._
+import scala.math.Ordering
 
 object WishService extends Wish.Encoders {
   def service(eventStore: EventStore): AuthedService[User] = AuthedService[User] {
     case AuthedRequest(u, GET -> Root / "wishlist" ) =>
-      eventStore.fetchEvents(u.id) map { es =>
-        es.foldRight(List.empty[Wish]) {
-          case (PutWishEvent(w, _), acc) => acc :+ w
-          case (_, acc) => acc
+      eventStore.fetchEvents(u.id) map { _.sortBy(_.time) } map { es =>
+        es.foldLeft(Map.empty[Wish.Id, Wish]) {
+          case (acc, PutWishEvent(w, _)) => acc + (w.id -> w)
+          case (acc, ForgetWishEvent(_, wid, _)) => acc - wid
         }
-      } map { _.asJson.noSpaces } >>= Ok[String]
+      } map { _.values.asJson.noSpaces } >>= Ok[String]
+  }
+
+  implicit val `ZonedDateTime has an Ordering` = new Ordering[ZonedDateTime] {
+    def compare(x: ZonedDateTime, y: ZonedDateTime): Int = x compareTo y
   }
 }
--- src/test/scala/WishServiceSpec.scala
+++ src/test/scala/WishServiceSpec.scala
@@ -1,3 +1,4 @@
+import java.time.ZonedDateTime
 import scalaz._
 import scalaz.concurrent.Task
 import org.scalatest._
@@ -6,6 +7,7 @@ import org.http4s.{Request, Method, AuthedRequest, Status, Uri}
 import org.http4s.circe._
 import io.circe._
 import io.circe.optics.JsonPath._
+import scala.util.Random
 
 class WishServiceSpec extends WordSpec with Matchers with MockitoSweetener with DataGenerators with Inside {
   "WishService" should {
@@ -23,6 +25,54 @@ class WishServiceSpec extends WordSpec with Matchers with MockitoSweetener with 
           root.id.string.getOption(w) shouldBe Some(wish.id.repr)
           root.uid.string.getOption(w) shouldBe Some(user.id.repr)
           root.title.string.getOption(w) shouldBe Some(wish.title)
+      }
+    }
+
+    "answer with updated wish" in new Fixture {
+      val t1 = ZonedDateTime.now
+      val origWish = newWish(user.id)
+
+      val t2 = t1.plusHours(1)
+      val updatedWish = origWish.copy(title = newTitle)
+      val events = Random.shuffle(PutWishEvent(origWish, t1) :: PutWishEvent(updatedWish, t2) :: Nil)
+
+      val request = Request(Method.GET, Uri(path = "/wishlist"))
+      val \/-(response) = service(AuthedRequest(user, request)).unsafePerformSyncAttempt
+      response.status shouldBe Status.Ok
+
+      val json = response.as[Json].unsafePerformSync
+      inside(root.arr.getOption(json)) {
+        case Some(w :: Nil) =>
+          root.title.string.getOption(w) shouldBe Some(updatedWish.title)
+      }
+    }
+
+    "answer with empty list when wish was deleted" in new Fixture {
+      val wish = newWish(user.id)
+      val t1 = ZonedDateTime.now
+      val t2 = t1.plusHours(1)
+      val events = Random.shuffle(PutWishEvent(wish, t1) :: ForgetWishEvent(user.id, wish.id, t2) :: Nil)
+
+      val request = Request(Method.GET, Uri(path = "/wishlist"))
+      val \/-(response) = service(AuthedRequest(user, request)).unsafePerformSyncAttempt
+      response.status shouldBe Status.Ok
+
+      val json = response.as[Json].unsafePerformSync
+      inside(root.arr.getOption(json)) {
+        case Some(xs) => xs shouldBe empty
+      }
+    }
+
+    "answer with empty list when no events exist" in new Fixture {
+      val events = Nil
+
+      val request = Request(Method.GET, Uri(path = "/wishlist"))
+      val \/-(response) = service(AuthedRequest(user, request)).unsafePerformSyncAttempt
+      response.status shouldBe Status.Ok
+
+      val json = response.as[Json].unsafePerformSync
+      inside(root.arr.getOption(json)) {
+        case Some(xs) => xs shouldBe empty
       }
     }
   }
